baca5229e4521662514885d46c308236f705a151
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-09-19 15:04:19 -0500

    :wrench: tsconfig.json config is updated correctly

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+/// <reference types="@angular/localize" />
+
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from 'src/app/app.module';
+import { environment } from 'src/environments/environment';
 
-import { AppModule } from './app/app.module';
-
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
