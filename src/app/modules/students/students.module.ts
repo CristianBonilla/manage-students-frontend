c088af58771990083f0f67b95a70c0fe330805d2
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-09-20 14:29:48 -0500

    :boom: Necessary modules for student module were imported

--- src/app/modules/students/students.module.ts
+++ src/app/modules/students/students.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DirectivesModule } from '@directives/directives.module';
 import { StudentService } from '@modules/students/services/student.service';
 import { studentsFeatureKey, reducer as studentsReducer } from '@modules/students/store';
 import { StudentEffects } from '@modules/students/store/effects/student.effects';
 import { StudentsRoutingModule } from '@modules/students/students-routing.module';
 import { StudentsComponent } from '@modules/students/students.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { IconsModule } from '@shared/icons/icons.module';
+import { NgxTrimDirectiveModule } from 'ngx-trim-directive';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
 
 @NgModule({
   declarations: [StudentsComponent],
@@ -14,7 +20,14 @@ import { StoreModule } from '@ngrx/store';
     CommonModule,
     StudentsRoutingModule,
     EffectsModule.forFeature([StudentEffects]),
-    StoreModule.forFeature(studentsFeatureKey, studentsReducer)
+    StoreModule.forFeature(studentsFeatureKey, studentsReducer),
+    IconsModule,
+    OverlayscrollbarsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxTrimDirectiveModule,
+    DirectivesModule
   ],
   providers: [StudentService]
 })
