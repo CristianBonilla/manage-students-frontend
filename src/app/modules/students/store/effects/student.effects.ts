61660dbb0575cf66732eebf5377e71899cc35960
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-09-20 14:26:43 -0500

    :boom: Added student store with effects to module

---
+++ src/app/modules/students/store/effects/student.effects.ts
@@ -0,0 +1,138 @@
+import { inject, Injectable } from '@angular/core';
+import { StudentService } from '@modules/students/services/student.service';
+import {
+  addStudentAction,
+  addStudentFailureAction,
+  addStudentSuccessAction,
+  deleteStudentAction,
+  deleteStudentFailureAction,
+  deleteStudentSuccessAction,
+  fetchStudentByIdAction,
+  fetchStudentByIdFailureAction,
+  fetchStudentByIdSuccessAction,
+  fetchStudentsAction,
+  fetchStudentsExcludedByTeacherAction,
+  fetchStudentsExcludedByTeacherFailureAction,
+  fetchStudentsExcludedByTeacherSuccessAction,
+  fetchStudentsFailureAction,
+  fetchStudentsSuccessAction,
+  hasAssociatedGradesByStudentAction,
+  hasAssociatedGradesByStudentFailureAction,
+  hasAssociatedGradesByStudentSuccessAction,
+  updateStudentAction,
+  updateStudentFailureAction,
+  updateStudentSuccessAction
+} from '@modules/students/store/actions/student.actions';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, delay, map, of, switchMap } from 'rxjs';
+import { DEFAULT_WAIT } from 'src/app/constants/common.constants';
+import { ServiceError } from 'src/app/models/service-error';
+
+@Injectable()
+export class StudentEffects {
+  readonly #actions = inject(Actions);
+  readonly #studentService = inject(StudentService);
+
+  addStudent$ = createEffect(() => this.#actions
+    .pipe(
+      ofType(addStudentAction),
+      delay(DEFAULT_WAIT),
+      switchMap(({ payload }) => this.#studentService.addStudent(payload)
+        .pipe(
+          map(student => addStudentSuccessAction({ student })),
+          catchError(httpError => {
+            const error: ServiceError = httpError.error ?? httpError;
+
+            return of(addStudentFailureAction({ error }));
+          })
+        ))
+    ));
+
+  updateStudent$ = createEffect(() => this.#actions
+    .pipe(
+      ofType(updateStudentAction),
+      delay(DEFAULT_WAIT),
+      switchMap(({ studentId, payload }) => this.#studentService.updateStudent(studentId, payload)
+        .pipe(
+          map(student => updateStudentSuccessAction({ student })),
+          catchError(httpError => {
+            const error: ServiceError = httpError.error ?? httpError;
+
+            return of(updateStudentFailureAction({ error }));
+          })
+        ))
+    ));
+
+  deleteStudent$ = createEffect(() => this.#actions
+    .pipe(
+      ofType(deleteStudentAction),
+      delay(DEFAULT_WAIT),
+      switchMap(({ studentId }) => this.#studentService.deleteStudent(studentId)
+        .pipe(
+          map(student => deleteStudentSuccessAction({ student })),
+          catchError(httpError => {
+            const error: ServiceError = httpError.error ?? httpError;
+
+            return of(deleteStudentFailureAction({ error }));
+          })
+        ))
+    ));
+
+  fetchStudents$ = createEffect(() => this.#actions
+    .pipe(
+      ofType(fetchStudentsAction),
+      delay(DEFAULT_WAIT),
+      switchMap(() => this.#studentService.fetchStudents()
+        .pipe(
+          map(students => fetchStudentsSuccessAction({ students })),
+          catchError(httpError => {
+            const error: ServiceError = httpError.error ?? httpError;
+
+            return of(fetchStudentsFailureAction({ error }));
+          })
+        ))
+    ));
+
+  fetchStudentById$ = createEffect(() => this.#actions
+    .pipe(
+      ofType(fetchStudentByIdAction),
+      delay(DEFAULT_WAIT),
+      switchMap(({ studentId }) => this.#studentService.fetchStudentById(studentId)
+        .pipe(
+          map(student => fetchStudentByIdSuccessAction({ student })),
+          catchError(httpError => {
+            const error: ServiceError = httpError.error ?? httpError;
+
+            return of(fetchStudentByIdFailureAction({ error }));
+          })
+        ))
+    ));
+
+  fetchStudentsExcludedByTeacher$ = createEffect(() => this.#actions
+    .pipe(
+      ofType(fetchStudentsExcludedByTeacherAction),
+      switchMap(({ teacherId }) => this.#studentService.fetchStudentsExcludedByTeacher(teacherId)
+        .pipe(
+          map(students => fetchStudentsExcludedByTeacherSuccessAction({ students })),
+          catchError(httpError => {
+            const error: ServiceError = httpError.error ?? httpError;
+
+            return of(fetchStudentsExcludedByTeacherFailureAction({ error }));
+          })
+        ))
+    ));
+
+  hasAssociatedGradesByStudent$ = createEffect(() => this.#actions
+    .pipe(
+      ofType(hasAssociatedGradesByStudentAction),
+      switchMap(({ studentId }) => this.#studentService.hasAssociatedGrades(studentId)
+        .pipe(
+          map(hasAssociatedGrades => hasAssociatedGradesByStudentSuccessAction({ hasAssociatedGrades })),
+          catchError(httpError => {
+            const error: ServiceError = httpError.error ?? httpError;
+
+            return of(hasAssociatedGradesByStudentFailureAction({ error }));
+          })
+        ))
+    ));
+}
--- src/app/modules/students/students.module.ts
+++ src/app/modules/students/students.module.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StudentService } from '@modules/students/services/student.service';
+import { studentsFeatureKey, reducer as studentsReducer } from '@modules/students/store';
+import { StudentEffects } from '@modules/students/store/effects/student.effects';
 import { StudentsRoutingModule } from '@modules/students/students-routing.module';
 import { StudentsComponent } from '@modules/students/students.component';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [StudentsComponent],
   imports: [
     CommonModule,
-    StudentsRoutingModule
+    StudentsRoutingModule,
+    EffectsModule.forFeature([StudentEffects]),
+    StoreModule.forFeature(studentsFeatureKey, studentsReducer)
   ],
   providers: [StudentService]
 })
