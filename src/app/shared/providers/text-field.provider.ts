1cea55b21c14f15b57b8b67876884e6a642ce948
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-09-22 01:50:14 -0500

    :recycle: Text field provider is improved to focus

--- src/app/shared/providers/text-field.provider.ts
+++ src/app/shared/providers/text-field.provider.ts
@@ -1,8 +1,13 @@
-import { ClassProvider, InjectionToken } from '@angular/core';
+import { ClassProvider, inject, InjectionToken } from '@angular/core';
+import { WINDOW } from '@core/providers/window.provider';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { asyncScheduler, filter, map, ReplaySubject, startWith, switchMap, take } from 'rxjs';
 import { TextFieldInfo } from 'src/app/models/text-field';
 
 export class TextField {
+  readonly #window = inject(WINDOW);
+  readonly #document = this.#window.document;
+  readonly #modal = inject(NgbModal);
   readonly #textField$ = new ReplaySubject<TextFieldInfo>(1);
 
   add(info: TextFieldInfo) {
@@ -13,11 +18,13 @@ export class TextField {
     this.#textField$
       .pipe(
         switchMap(info => info.control.statusChanges.pipe(startWith(null), map(_ => info))),
-        filter(({ control }) => control.enabled),
+        filter(({ control, $text }) => control.enabled && this.#document.activeElement !== $text),
         take(1)
       ).subscribe(({ $text }) => {
         asyncScheduler.schedule(() => {
-          $text.focus();
+          if (!this.#modal.hasOpenModals()) {
+            $text.focus();
+          }
         });
       });
   }
