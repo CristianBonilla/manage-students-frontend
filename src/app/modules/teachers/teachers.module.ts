8702500d6dad5f877766b5b80afa43727ba2e613
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-09-23 10:18:33 -0500

    :boom: Modules required for the teachers module are included

--- src/app/modules/teachers/teachers.module.ts
+++ src/app/modules/teachers/teachers.module.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DirectivesModule } from '@directives/directives.module';
 import { TeacherService } from '@modules/teachers/services/teacher.service';
 import { teachersFeatureKey, reducer as teachersReducer } from '@modules/teachers/store';
 import { TeacherEffects } from '@modules/teachers/store/effects/teacher.effects';
 import { TeachersRoutingModule } from '@modules/teachers/teachers-routing.module';
 import { TeachersComponent } from '@modules/teachers/teachers.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { IconsModule } from '@shared/icons/icons.module';
+import { TEXT_FIELD_PROVIDER } from '@shared/providers/text-field.provider';
+import { NgxTrimDirectiveModule } from 'ngx-trim-directive';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
 
 @NgModule({
   declarations: [TeachersComponent],
@@ -15,7 +22,14 @@ import { StoreModule } from '@ngrx/store';
     TeachersRoutingModule,
     EffectsModule.forFeature([TeacherEffects]),
     StoreModule.forFeature(teachersFeatureKey, teachersReducer),
+    IconsModule,
+    OverlayscrollbarsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxTrimDirectiveModule,
+    DirectivesModule
   ],
-  providers: [TeacherService]
+  providers: [TeacherService, TEXT_FIELD_PROVIDER]
 })
 export class TeachersModule { }
