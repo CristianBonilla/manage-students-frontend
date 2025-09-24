a49839d1a4364d050fea6b4d74122d4cdb2c4430
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-09-24 01:52:51 -0500

    :construction: Necessary modules are imported into the grades module

--- src/app/modules/grades/grades.module.ts
+++ src/app/modules/grades/grades.module.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DirectivesModule } from '@directives/directives.module';
 import { GradesRoutingModule } from '@modules/grades/grades-routing.module';
 import { GradesComponent } from '@modules/grades/grades.component';
 import { GradeService } from '@modules/grades/services/grade.service';
 import { gradesFeatureKey, reducer as gradesReducer } from '@modules/grades/store';
 import { GradeEffects } from '@modules/grades/store/effects/grade.effects';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { IconsModule } from '@shared/icons/icons.module';
+import { TEXT_FIELD_PROVIDER } from '@shared/providers/text-field.provider';
+import { NgxTrimDirectiveModule } from 'ngx-trim-directive';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
 
 @NgModule({
   declarations: [GradesComponent],
@@ -15,7 +22,14 @@ import { StoreModule } from '@ngrx/store';
     GradesRoutingModule,
     EffectsModule.forFeature([GradeEffects]),
     StoreModule.forFeature(gradesFeatureKey, gradesReducer),
+    IconsModule,
+    OverlayscrollbarsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxTrimDirectiveModule,
+    DirectivesModule
   ],
-  providers: [GradeService]
+  providers: [GradeService, TEXT_FIELD_PROVIDER]
 })
 export class GradesModule { }
